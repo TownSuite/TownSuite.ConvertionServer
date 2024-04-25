6522d5e588866f29ae6508499c6c3edad047ef62
TrevorDelaney <trevorbdelaney@gmail.com>
2024-04-25 10:47:48 -0230

    Ensure stream cannot be too large
    A maximum stream size can be given to the CopyToAsync method of the UploadedStreamHandler class. The method will throw an exception if the Stream to copy from has more byte's after copying the maximum amount of bytes.
    
    The CopyToAsync method can also take in a CancellationToken now.
    
    These changes involved a lot of change to how bytes are copied from 1 stream to another. The logic is more manual now in that it doesn't use the CopyToAsync method of the Stream class and instead copies in to a byte array (buffer) and then writes that data to the other stream. The current max buffer size (per iteration) is 80000 which was choosen because it is close to the default for Stream's "CopyToAsync" method of 81920.
    Issue: https://github.com/TownSuite/TownSuite.IssueTracker/issues/7288

--- TownSuite.ConversionServer/TownSuite.ConversionServer.Common/Validation/UploadedStreamHandler.cs
+++ TownSuite.ConversionServer/TownSuite.ConversionServer.Common/Validation/UploadedStreamHandler.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TownSuite.ConversionServer.Common.Models.Errors;
+using TownSuite.ConversionServer.Interfaces.Common.Bytes;
 
 namespace TownSuite.ConversionServer.Common.Validation
 {
     public class UploadedStreamHandler
     {
         private readonly Stream _stream;
+        private const int MAX_BUFFER_SIZE = 80000;
 
         public UploadedStreamHandler(Stream stream)
         {
             _stream = stream;
         }
 
-        public async Task CopyToAsync(Stream streamToCopyTo)
+        public async Task CopyToAsync(Stream streamToCopyTo, IByteCount maxBytes, CancellationToken token)
         {
-            await _stream.CopyToAsync(streamToCopyTo);
+            var totalBytesRead = 0L;
+            var lastRead = -1;
+            while (lastRead != 0 && maxBytes.Bytes > totalBytesRead) 
+            {
+                var buffer_length = (int)Math.Min(MAX_BUFFER_SIZE, maxBytes.Bytes - totalBytesRead);
+                var buffer = new byte[buffer_length];
+                lastRead = await _stream.ReadAsync(buffer, 0, buffer_length, token);
+                await streamToCopyTo.WriteAsync(buffer, 0, lastRead, token);
+                totalBytesRead += lastRead;
+                token.ThrowIfCancellationRequested();
+            }
+
+            if (_stream.ReadByte() != -1)
+            {
+                throw new ValidationException($"PDF SIZE TOO LARGE. Greater than {maxBytes.Megabytes} megabytes.");
+            }
             streamToCopyTo.Seek(0, SeekOrigin.Begin);
             EnsureStreamHasData(streamToCopyTo);
         }
--- TownSuite.ConversionServer/TownSuite.ConversionServer.Tests.GeneralTests/Utilities/Validation/UploadedStreamHandlerTests.cs
+++ TownSuite.ConversionServer/TownSuite.ConversionServer.Tests.GeneralTests/Utilities/Validation/UploadedStreamHandlerTests.cs
@@ -5,22 +5,42 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TownSuite.ConversionServer.Common.Bytes;
 using TownSuite.ConversionServer.Common.Models.Errors;
 using TownSuite.ConversionServer.Common.Validation;
+using TownSuite.ConversionServer.Interfaces.Common.Bytes;
 
 namespace TownSuite.ConversionServer.Tests.GeneralTests.Utilities.Validation
 {
     [TestFixture]
     public class UploadedStreamHandlerTests
     {
+        private IByteCount _defaultByteCount;
+        private Stream _streamToCopyTo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _defaultByteCount = new ByteCount()
+            {
+                Megabytes = 10
+            };
+            _streamToCopyTo = new MemoryStream();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _streamToCopyTo.Dispose();
+        }
+
         [Test]
         public void Does_ensure_stream_has_data()
         {
             using var streamToCopyFrom = new MemoryStream([1]);
-            using var streamToCopyTo = new MemoryStream();
 
             var streamValidator = new UploadedStreamHandler(streamToCopyFrom);
-            var test = () => streamValidator.CopyToAsync(streamToCopyTo);
+            var test = () => streamValidator.CopyToAsync(_streamToCopyTo, _defaultByteCount, default);
 
             Assert.That(test, Throws.Nothing);
         }
@@ -29,10 +49,24 @@ namespace TownSuite.ConversionServer.Tests.GeneralTests.Utilities.Validation
         public void Does_throw_error_when_no_data_in_stream()
         {
             using var streamToCopyFrom = new MemoryStream([]);
-            using var streamToCopyTo = new MemoryStream();
 
             var streamValidator = new UploadedStreamHandler(streamToCopyFrom);
-            var test = () => streamValidator.CopyToAsync(streamToCopyTo);
+            var test = () => streamValidator.CopyToAsync(_streamToCopyTo, _defaultByteCount, default);
+
+            Assert.That(test, Throws.InstanceOf<ValidationException>());
+        }
+
+        [Test]
+        public void Does_throw_error_when_stream_is_too_large()
+        {
+            using var streamToCopyFrom = new RestrictedStream([0x1, 0x2, 0x3]);
+            var maxBytes = new ByteCount()
+            {
+                Bytes = 2
+            };
+
+            var streamValidator = new UploadedStreamHandler(streamToCopyFrom);
+            var test = () => streamValidator.CopyToAsync(_streamToCopyTo, maxBytes, default);
 
             Assert.That(test, Throws.InstanceOf<ValidationException>());
         }
@@ -41,13 +75,12 @@ namespace TownSuite.ConversionServer.Tests.GeneralTests.Utilities.Validation
         public async Task Does_copy_stream_data()
         {
             using var streamToCopyFrom = new RestrictedStream([0x1]);
-            using var streamToCopyTo = new MemoryStream();
 
             var streamValidator = new UploadedStreamHandler(streamToCopyFrom);
-            await streamValidator.CopyToAsync(streamToCopyTo);
+            await streamValidator.CopyToAsync(_streamToCopyTo, _defaultByteCount, default);
 
-            Assert.That(streamToCopyTo.Length, Is.EqualTo(1));
-            Assert.That(streamToCopyTo.ReadByte(), Is.EqualTo(0x1));
+            Assert.That(_streamToCopyTo.Length, Is.EqualTo(1));
+            Assert.That(_streamToCopyTo.ReadByte(), Is.EqualTo(0x1));
         }
     }
 }
